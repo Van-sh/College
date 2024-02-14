a38621c1feb6e81c625747417b1c71813f5d5489
Vansh <72124535+Van-sh@users.noreply.github.com>
2024-02-14 13:19:40 +0000

    Fix geometricSum.c

--- C/Third Week/geometricSum.c
+++ C/Third Week/geometricSum.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 void main()
 {
-    for (int i = 1; i <= 10; i++)
+    int a, r, n, sum = 0;
+    printf("Enter a, r and n: ");
+    scanf("%i %i %i", &a, &r, &n);
+    for (int i = 0; i < n; i++)
     {
-        printf("%i\n", i);
+        sum += a * pow(r, i);
     }
+    printf("Sum of %i numbers of this geometric series is %i\n", n, sum);
 }
