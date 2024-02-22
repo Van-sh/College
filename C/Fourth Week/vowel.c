a07e8b7ad005bb8d01561007c644f254c4649fb8
Vansh <72124535+Van-sh@users.noreply.github.com>
2024-02-22 09:02:31 +0000

    Refactor

--- C/Fourth Week/vowel.c
+++ C/Fourth Week/vowel.c
@@ -8,17 +8,9 @@ void main()
     switch (ch)
     {
     case 'a':
-        printf("Vowel\n");
-        break;
     case 'e':
-        printf("Vowel\n");
-        break;
     case 'i':
-        printf("Vowel\n");
-        break;
     case 'o':
-        printf("Vowel\n");
-        break;
     case 'u':
         printf("Vowel\n");
         break;
