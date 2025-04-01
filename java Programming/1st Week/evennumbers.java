e8155a92424c4e135bd199817abcdd89dfc55787
Vansh <72124535+Van-sh@users.noreply.github.com>
2025-04-01 17:16:08 +0000

    fix file names

--- java Programming/1st Week/evennumbers.java
+++ java Programming/1st Week/evennumbers.java
@@ -1,5 +1,5 @@
 // 6. Write a Program to print even numbers between 23 and 57. Each numbers should be printed in a separate row.
-public class numbers {
+public class evennumbers {
    public static void main(String[] args) {
       for (int i = 23; i <= 57; i++) {
          System.out.println(i);
