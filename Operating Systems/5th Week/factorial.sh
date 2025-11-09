993aee105743178af3e55dd17ff338260f50402b
Vansh | 1sh <72124535+Van-sh@users.noreply.github.com>
2025-11-09 14:03:12 +0000

    Fix

--- Operating Systems/5th Week/factorial.sh
+++ Operating Systems/5th Week/factorial.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # 10. Write a program to calculate the factorial of a given number.
 
-if [[ $# != 1 ]]; then
+if (( $# != 1 )) ; then
    echo "Usage: $0 <number>"
    exit 1
 fi
 
-if [[ $1 < 0 ]]; then
+if (( $1 < 0 )); then
    echo "number must be greater than 0"
    exit 2
 fi
